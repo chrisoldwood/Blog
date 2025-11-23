dab5f53f915438045f9cf5a03e245a285c76c790
Chris Oldwood <gort@cix.co.uk>
2025-11-23 17:57:29 +0000

    Added a script to list the URL for each blog post

---
+++ Tools/ListBlogPosts.ps1
@@ -0,0 +1,52 @@
+param (
+    [Parameter(Mandatory=$true, Position=0)]
+    [string] $URL,
+    [Parameter(Mandatory=$false)]
+    [switch] $AsConfig
+)
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+If (-not (Get-Module -ErrorAction Ignore -ListAvailable PSParseHTML)) {
+  Write-Host "Installing PSParseHTML module for the current user..."
+  Install-Module -Scope CurrentUser PSParseHTML
+}
+
+$dom = ConvertFrom-Html -Engine AngleSharp -Url $URL
+$history = $dom.QuerySelectorAll('.archivedate') | foreach {
+    $href = $_.QuerySelector('.post-count-link').href
+    if ($href -match '\d{4}/\d{2}/$') {
+        $href
+    }
+}
+
+function Reverse
+{ 
+    $array = @($input)
+    [array]::reverse($array)
+    $array
+}
+
+$pages = $history | sort | select -First 2 | foreach {
+    $dom = ConvertFrom-Html -Engine AngleSharp -Url $_
+    $links = $dom.QuerySelectorAll('.archivedate .expanded') | foreach {
+        $_.QuerySelectorAll('.posts') | foreach {
+            $_.QuerySelectorAll('li') | foreach {
+                $_.QuerySelectorAll('a') | foreach {
+                    $_.href
+                }
+            }
+        }
+    }
+    $links | Reverse
+}
+
+$pages | foreach {
+    if ($AsConfig) {
+        $date = & { $_ -match '/(\d{4}/\d{2})/' | Out-Null; $Matches[1] }
+        $title = & { $_ -match '/([a-z0-9-]+).html$' | Out-Null; $Matches[1] }
+        "    @{{ Date = '{0}'; Input='{1}.html'; Output='{1}.md' }}" -f $date,$title
+    } else {
+        $_
+    }
+}
