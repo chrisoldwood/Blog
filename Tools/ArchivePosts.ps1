b1223ea5e9fd935c4a6ab8f389e7fab40e1e933a
Chris Oldwood <gort@cix.co.uk>
2025-11-23 21:07:33 +0000

    Factored out the posts config so we can reuse it for the ToC generator

--- Tools/ArchivePosts.ps1
+++ Tools/ArchivePosts.ps1
@@ -1,16 +1,11 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-$posts = @(
-    @{ InputDate = '2009/04'; InputUrl='apology-to-raymond-chen.html'; OutputDate = '2009/04'; OutputFile='an-apology-to-raymond-chen.md' }
-    @{ InputDate = '2009/05'; InputUrl='who-is-accu.html'; OutputDate = '2009/05'; OutputFile='who-is-the-accu.md' }
-    @{ InputDate = '2009/05'; InputUrl='potted-history-of-my-windows-framework.html'; OutputDate = '2009/05'; OutputFile='potted-history-of-my-windows-framework.md' }
-    @{ InputDate = '2009/05'; InputUrl='bcs-talk-introduction-to-f.html'; OutputDate = '2009/05'; OutputFile='bcs-talk-introduction-to-fsharp.md' }
-)
+. (Join-Path (Split-Path -Path $MyInvocation.MyCommand.Path) Posts.ps1)
 
 $baseUrl = 'https://chrisoldwood.blogspot.com'
 
-$posts | ForEach-Object {
+$Posts | ForEach-Object {
     $inputUrl = "$($_.InputDate)/$($_.InputUrl)"
     $outputPath = "$($_.OutputDate)/$($_.OutputFile)"
 
---
+++ Tools/Posts.ps1
@@ -0,0 +1,6 @@
+$Posts = @(
+    @{ InputDate = '2009/04'; InputUrl='apology-to-raymond-chen.html'; OutputDate = '2009/04'; OutputFile='an-apology-to-raymond-chen.md' }
+    @{ InputDate = '2009/05'; InputUrl='who-is-accu.html'; OutputDate = '2009/05'; OutputFile='who-is-the-accu.md' }
+    @{ InputDate = '2009/05'; InputUrl='potted-history-of-my-windows-framework.html'; OutputDate = '2009/05'; OutputFile='potted-history-of-my-windows-framework.md' }
+    @{ InputDate = '2009/05'; InputUrl='bcs-talk-introduction-to-f.html'; OutputDate = '2009/05'; OutputFile='bcs-talk-introduction-to-fsharp.md' }
+)
