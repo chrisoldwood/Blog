516db8d25e97af785269948d4c5235718f50e563
Chris Oldwood <gort@cix.co.uk>
2026-01-01 20:16:24 +0000

    Added date filtering when archiving blog posts

--- Tools/ArchivePosts.ps1
+++ Tools/ArchivePosts.ps1
@@ -1,15 +1,30 @@
+param (
+    [Parameter(Mandatory=$false)]
+    [string] $BaseURL = 'https://chrisoldwood.blogspot.com',
+    [Parameter(Mandatory=$false)]
+    [string] $FromDate = '2000/01',
+    [Parameter(Mandatory=$false)]
+    [string] $ToDate = '3000/01'
+)
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
+if ( ($FromDate -notmatch '^\d{4}/\d{2}$') -or ($ToDate -notmatch '^\d{4}/\d{2}$') ) {
+    Write-Error "-FromDate and -ToDate must be in the format 'YYYY/MM'."
+}
+
 . (Join-Path (Split-Path -Path $MyInvocation.MyCommand.Path) Posts.ps1)
 
-$baseUrl = 'https://chrisoldwood.blogspot.com'
+function InDateRange([string] $date, [string] $from, [string] $to)
+{
+    ($date -ge $from) -and ($date -lt $to)
+}
 
-$Posts | foreach {
+$Posts | where { InDateRange $_.OutputDate $FromDate $ToDate } | foreach {
     $inputUrl = "$($_.InputDate)/$($_.InputUrl)"
     $outputPath = "$($_.OutputDate)/$($_.OutputFile)"
 
     Write-Information "Archiving '$inputUrl' as '$outputPath'" -InformationAction Continue
     if (!(Test-Path $_.OutputDate)) { mkdir $_.OutputDate | Out-Null }
-    .\Tools\BlogPostToMarkdown.ps1 "$baseUrl/$inputUrl" "$outputPath"
+    .\Tools\BlogPostToMarkdown.ps1 "$BaseURL/$inputUrl" "$outputPath"
 }
