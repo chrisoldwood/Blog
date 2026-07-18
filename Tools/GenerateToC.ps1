72427f99b947f4558eb1fac48a6e46673d8dea78
Chris Oldwood <gort@cix.co.uk>
2026-07-18 19:29:50 +0100

    Added a date filter to the ToC generator script

--- Tools/GenerateToC.ps1
+++ Tools/GenerateToC.ps1
@@ -1,5 +1,16 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$false)]
+    [string] $FromDate = '2000/01',
+    [Parameter(Mandatory=$false)]
+    [string] $ToDate = '3000/01'
+)
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
+
+if ( ($FromDate -notmatch '^\d{4}/\d{2}$') -or ($ToDate -notmatch '^\d{4}/\d{2}$') ) {
+    Write-Error "-FromDate and -ToDate must be in the format 'YYYY/MM'."
+}
 
 . (Join-Path (Split-Path -Path $MyInvocation.MyCommand.Path) Posts.ps1)
 
@@ -10,9 +21,14 @@ function Reverse
     $array
 }
 
+function InDateRange([string] $date, [string] $from, [string] $to)
+{
+    ($date -ge $from) -and ($date -lt $to)
+}
+
 $section = $null
 
-$Posts | Reverse | foreach {
+$Posts | where { InDateRange $_.OutputDate $FromDate $ToDate } | Reverse | foreach {
     $year = $_.InputDate.Substring(0, 4)
     if ($section -ne $year) {
         $section = $year
